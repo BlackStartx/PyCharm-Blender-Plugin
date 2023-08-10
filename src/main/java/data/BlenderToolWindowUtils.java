ff01db816d1867e52c758e804f2eb3fde14b207e
Jonas Holzman <jonas@holzman.fr>
2023-08-10 18:59:45 +0200

    Fix broken Debug mode due to bad .egg path

--- src/main/java/data/BlenderToolWindowUtils.java
+++ src/main/java/data/BlenderToolWindowUtils.java
@@ -1,25 +1,12 @@
 package data;
 
+import com.intellij.openapi.application.PathManager;
+
 import java.io.File;
-import java.net.URL;
 
 public class BlenderToolWindowUtils {
-
-    public static File getJarPath() {
-        URL x = BlenderToolWindowUtils.class.getClassLoader().getResource("debugger");
-        if (x == null) return null;
-        return new File(x.getPath().split("/", 2)[1].split(".jar!")[0] + ".jar");
-    }
-
-    public static File getIdePath() {
-        File jar = getJarPath();
-        if (jar == null) return null;
-        return getJarPath().getParentFile().getParentFile();
-    }
-
     public static File getEggFile() {
-        File ide = getIdePath();
-        if (ide == null) return null;
-        return new File(ide.getPath() + File.separator + "debug-eggs" + File.separator + "pydevd-pycharm.egg");
+        String ide = PathManager.getHomePath();
+        return new File(ide + File.separator + "debug-eggs" + File.separator + "pydevd-pycharm.egg");
     }
 }
