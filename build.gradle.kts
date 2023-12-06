734fcf4cfdc1bf02adf9e09a44a85c82b0f43145
BlackStartx <leonardo.lorenzoni99@gmail.com>
2023-12-06 23:05:20 +0100

    [2023.3] Blend-Charm 2023.3 release~

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.14.1"
+    id("org.jetbrains.intellij") version "1.16.1"
 }
 
 group = "org.blackstartx"
 val code = "isCommunity = true"
 val path = "src\\main\\java\\plugin_settings\\PluginSettings.java"
 val type = if (File(path).readText().contains(code)) "Community" else "Professional"
-version = "2023.2.1"
+version = "2023.3"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -19,13 +19,13 @@ repositories {
 
 dependencies {
     testImplementation(group = "junit", name = "junit", version = "4.13.2")
-    implementation(group = "org.json", name = "json", version = "20230227")
+    implementation(group = "org.json", name = "json", version = "20231013")
 }
 
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     plugins.set(listOf("python"))
-    version.set("PY-2023.2")
+    version.set("PY-2023.3")
 }
 tasks {
     buildPlugin {
