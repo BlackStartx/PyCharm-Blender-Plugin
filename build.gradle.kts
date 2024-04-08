10d018610d8201773893040f7814f174dae4a048
BlackStartx <leonardo.lorenzoni99@gmail.com>
2024-04-08 05:42:02 +0200

    [2024.1] Blend-Charm 2024.1 release~

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.16.1"
+    id("org.jetbrains.intellij") version "1.17.3"
 }
 
 group = "org.blackstartx"
 val code = "isCommunity = true"
 val path = "src\\main\\java\\plugin_settings\\PluginSettings.java"
 val type = if (File(path).readText().contains(code)) "Community" else "Professional"
-version = "2023.3"
+version = "2024.1"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -18,14 +18,14 @@ repositories {
 }
 
 dependencies {
-    testImplementation(group = "junit", name = "junit", version = "4.13.2")
-    implementation(group = "org.json", name = "json", version = "20231013")
+    testImplementation(group = "junit", name = "junit", version = "5.10.2")
+    implementation(group = "org.json", name = "json", version = "20240303")
 }
 
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     plugins.set(listOf("python"))
-    version.set("PY-2023.3")
+    version.set("PY-2024.1")
 }
 tasks {
     buildPlugin {
