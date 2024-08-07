7e3edf02bce8809f436f4c630121f190b4f52b33
BlackStartx <leonardo.lorenzoni99@gmail.com>
2024-08-07 22:11:08 +0200

    [Gradle] Update~

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.17.3"
+    id("org.jetbrains.intellij") version "1.17.4"
 }
 
 group = "org.blackstartx"
@@ -9,16 +9,12 @@ val path = "src\\main\\java\\plugin_settings\\PluginSettings.java"
 val type = if (File(path).readText().contains(code)) "Community" else "Professional"
 version = "2024.1"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-}
-
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(group = "junit", name = "junit", version = "5.10.2")
+    testImplementation(group = "junit", name = "junit", version = "4.13.2")
     implementation(group = "org.json", name = "json", version = "20240303")
 }
 
@@ -27,9 +23,16 @@ intellij {
     plugins.set(listOf("python"))
     version.set("PY-2024.1")
 }
+
 tasks {
     buildPlugin {
         archiveFileName.set("${rootProject.name}.${version}.${type}.Edition.zip")
     }
+
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+
     patchPluginXml {}
 }
